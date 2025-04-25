ddbfbbdca34648f5dfe1e360e860111401bc99b8
Crystal <crystal.kimalel@gmail.com>
2025-04-25 13:44:58 +0300

    Add Sum of elements in a list file

---
+++ Sum_of_elements_in_list.py
@@ -0,0 +1,12 @@
+#list with integer
+integer = [1,2,3,4,5,6,7,8,9,10]
+#sum to add the elemnts in integer list
+sum_integer=sum(integer)
+#prints the total sum of elements
+print(sum_integer)
+
+#list with decimals
+float_point=[0.5,1.5,2.5,3.5,4.5,5.5]
+#Adding the decimals
+sum_float_point=sum(float_point)
+print(sum_float_point)

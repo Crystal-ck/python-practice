5f00c1770d2d5611c2a3236a2f24b70c067faea9
Crystal <crystal.kimalel@gmail.com>
2025-04-25 13:59:23 +0300

    Add Even or Odd file

---
+++ Even_or_Odd.py
@@ -0,0 +1,17 @@
+
+# if loop to check if the the modulus of num divides by 2 is 0 making it even otherwise its odd
+num= 3
+if num%2==0:
+    print("The number is Even")
+    
+else:
+    print("The number is Odd")
+
+
+#using user input to enter the number
+num= int(input("Enter any number: "))
+if num%2==0:
+    print("The number is Even")
+    
+else:
+    print("The number is Odd")
